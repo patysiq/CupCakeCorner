8ef950666186bac781d4086441131aa606e2bf34
Patricia <patysiq@yahoo.com.br>
2021-02-20 20:57:16 -0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by PATRICIA S SIQUEIRA on 20/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
